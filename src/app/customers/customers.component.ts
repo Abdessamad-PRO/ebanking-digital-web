d481a8fb4fe55192bfb60a5a36e06216b9baa393
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-10 12:07:57 +0100

    Création des service et ijecter les dependances

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CustomerService } from '../services/customer.service';
 @Component({
   selector: 'app-customers',
   standalone: false,
@@ -10,16 +11,26 @@ import { HttpClient } from '@angular/common/http';
 export class CustomersComponent implements OnInit {
   customers: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private customerService:CustomerService) { }
 
+  // ngOnInit(): void {
+  //   this.http.get('http://localhost:8085/customers').subscribe(data=>{
+  //     //  stocker les données dans un objet data
+  //     this.customers=data;
+  //   },error=>{
+  //     console.log(error);
+  //   })
+
+  // }
+  
   ngOnInit(): void {
-    this.http.get('http://localhost:8085/customers').subscribe(data=>{
-      //  stocker les données dans un objet data
-      this.customers=data;
-    },error=>{
-      console.log(error);
-    })
-
-  }
-
+  this.customerService.getCustomers().subscribe({
+    next: (data) => {
+      this.customers = data;
+    },
+    error: (err) => {
+      console.log(err);
+    }
+  }); 
+}
 }
