d6dfb573417ea35fc2d3e664acf9cd75ef540e49
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-10 11:43:47 +0100

    Affichage de la liste des clients et reglage de probleme de cross policy

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-customers',
   standalone: false,
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  customers: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8085/customers').subscribe(data=>{
+      //  stocker les données dans un objet data
+      this.customers=data;
+    },error=>{
+      console.log(error);
+    })
+
+  }
 
 }
